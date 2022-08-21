806413c3469979489d2016e0ce45f391139e2c1d
HarjeetPadda <harjeetpadda@gmail.com>
2022-08-21 15:54:19 +1000

    test js

--- js/thanks.js
+++ js/thanks.js
@@ -2,6 +2,6 @@ window.addEventListener('load',() => {
     
     const name = sessionStorage.getItem('NAME');
 
-    document.getElementById('result-name').innerHTML = name[0].toUpperCase()+name[1].toLowerCase();
+    document.getElementById('result-name').innerHTML = name[0].toUpperCase()+name[1,].toLowerCase();
 
 })
